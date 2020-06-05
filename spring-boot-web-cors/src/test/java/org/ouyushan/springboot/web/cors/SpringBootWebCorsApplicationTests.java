fdb1409d7e97b12ac21e3c88ecb27d0c3843d048
欧虞山 <ouyushan@hotmail.com>
2020-06-05 16:02:45 +0800

    initial
    initial

---
+++ spring-boot-web-cors/src/test/java/org/ouyushan/springboot/web/cors/SpringBootWebCorsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ouyushan.springboot.web.cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebCorsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
